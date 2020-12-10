f0603813df0266ebf031ea69b8834bfbb0a7e63f
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-12-10 16:20:30 -0400

    Add todo displaying inside projects

--- src/display.js
+++ src/display.js
@@ -1,18 +1,22 @@
 const displayProjectTodo = (projectArr, todoArr, content) => {
-  projectArr.map(item => {
+  projectArr.map(projectTitle => {
     const projectDiv = document.createElement('div');
     projectDiv.setAttribute('class', 'project-div');
     projectDiv.innerHTML = `
-    <h1>${item}</h1>
+    <h1>${projectTitle}</h1>
     <div class="todos-div">
-      <ul class="todo-info">
-        <li>${todoArr[0].title}</li>
-        <li>${todoArr[0].description}</li>
-        <li>${todoArr[0].dueDate}</li>
-        <li>${todoArr[0].priority}</li>
-      </ul>
-    </div>
-    `;
+    ${todoArr.map((todo, index) => {
+    if (todo.project === projectTitle) {
+      return `
+      <ul class="todo-info" id="${index}">
+      <li>${todo.title}</li>
+      <li>${todo.description}</li>
+      <li>${todo.dueDate}</li>
+      <li>${todo.priority}</li>
+      </ul>`;
+    }
+  }).join('')}
+    </div>`;
 
     content.appendChild(projectDiv);
   });
