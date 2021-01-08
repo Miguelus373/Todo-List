63a92f38efaecc5d63da3982e1e60a9a89215da7
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2021-01-08 17:53:35 -0400

    Add setStorage tests

--- src/set-storage.test.js
+++ src/set-storage.test.js
@@ -1,7 +1,16 @@
 import setStorage from './set-storage';
 
-describe('set Storage', () => {
-  it('returns undefined if the value is given', () => {
-    expect(setStorage('str', ['projects'])).toBeUndefined();
-  });
-});
+describe('Set Storage', () => {
+  it('Sets an item to the local storage', () => {
+    setStorage('projectList', ['Global']);
+
+    expect(localStorage.getItem('projectList')).toBe("[\"Global\"]")
+  })
+
+  it("Doesn's set and item if it already exists in the local storage", () => {
+    localStorage.setItem('projectList', "[\"Global\"]")
+    setStorage('projectList', ['Different Project']);
+    
+    expect(localStorage.getItem('projectList')).toBe("[\"Global\"]")
+  })
+})
