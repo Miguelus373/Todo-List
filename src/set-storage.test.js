76a1df621a41f274696007ad2babcebd977680d5
chinweokwu <paulcmorah@gmail.com>
2021-01-08 23:03:21 +0100

    made changes to storage js

--- src/set-storage.test.js
+++ src/set-storage.test.js
@@ -4,13 +4,12 @@ describe('Set Storage', () => {
   it('Sets an item to the local storage', () => {
     setStorage('projectList', ['Global']);
 
-    expect(localStorage.getItem('projectList')).toBe("[\"Global\"]")
-  })
+    expect(localStorage.getItem('projectList')).toBe('["Global"]');
+  });
 
   it("Doesn's set and item if it already exists in the local storage", () => {
-    localStorage.setItem('projectList', "[\"Global\"]")
+    localStorage.setItem('projectList', '["Global"]');
     setStorage('projectList', ['Different Project']);
-    
-    expect(localStorage.getItem('projectList')).toBe("[\"Global\"]")
-  })
-})
+    expect(localStorage.getItem('projectList')).toBe('["Global"]');
+  });
+});
